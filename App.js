f56f8b689972456df912cdfa351727df0dd8ae6b
MarksChiele <marcos.vcd@outlook.com>
2023-06-03 18:11:26 -0300

    Ajuste da App,js para chamar a Home.js

--- App.js
+++ App.js
@@ -1,66 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import{NavigationContainer} from '@react-navigation/native';
+import{createStackNavigator} from '@react-navigation/stack';
+import{Home} from './views';
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanning, setScanning] = useState(false);
-  const [scannedData, setScannedData] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-
-  const handleBarCodeScanned = ({ data }) => {
-    setScanning(false);
-    setScannedData(data);
-  };
-
-  const startScan = () => {
-    setScanning(true);
-    setScannedData(null);
-  };
-
-  if (hasPermission === null) {
-    return <Text>Solicitando permissão de acesso à câmera...</Text>;
-  }
-
-  if (hasPermission === false) {
-    return <Text>Acesso à câmera negado.</Text>;
-  }
-
-  return (
-    <View style={styles.container}>
-      {scanning ? (
-        <BarCodeScanner
-          onBarCodeScanned={handleBarCodeScanned}
-          style={StyleSheet.absoluteFillObject}
-        />
-      ) : (
-        <View style={styles.dataContainer}>
-          <Text style={styles.dataText}>{scannedData}</Text>
-          <Button title="Escanear QR Code" onPress={startScan} />
-        </View>
-      )}
-    </View>
-  );
+return(
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home}/>
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  dataContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  dataText: {
-    fontSize: 20,
-    marginBottom: 10,
-  },
-});
--- dao/item.php
+++ dao/item.php
@@ -1,16 +1,17 @@
 <?php
+
 include_once "./conexao/conexao.php";
 
-function criaItem($descricao, $tipo, $idcliente)
+function buscaItem($iditem)// iditem e o valor capturado pelo QRCode
 {
     try {
         $conn = criaConexao();
 
-        $sql = "INSERT INTO stock.produto (descricao, tipo, idcliente) VALUES(?, ?) ";
+        $sql = "SELECT * FROM stock.produto where idproduto=? ";
 
         $stmt = $conn->prepare($sql);
 
-        $stmt->execute([$descricao, $tipo, $idcliente]);
+        $stmt->execute([$iditem]);
 
         $conn = null;
 
@@ -22,7 +23,28 @@ function criaItem($descricao, $tipo, $idcliente)
     }
 }
 
-function atualizastockItem($idproduto, $qtde)
+function buscaCliente()
+{
+    try {
+        $conn = criaConexao();
+
+        $sql = "SELECT * FROM stock.cliente where idcliente=? ";
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->execute();
+
+        $conn = null;
+
+        return true;
+
+    } catch (PDOException $e) {
+        print($e->getMessage());
+        return false;
+    }
+}
+
+function atualizastockItem($idproduto, $qtde) //qtde e a quantidade nova do produto calculada pela funcao.... que fara a soma ou subtracao da quantidade atual X o digitado pelo usuario
 {
     try {
         $conn = criaConexao();
