f691d982176af4c7be7e8f447be0db8d9b95098e
MarksChiele <marcos.vcd@outlook.com>
2023-06-21 19:22:28 -0300

    alteracoes
    alteracoes

--- assets/Css/Css.js
+++ assets/Css/Css.js
@@ -2,96 +2,106 @@ import { StyleSheet } from "react-native";
 
 
 const css = StyleSheet.create({
-    container:{
-        flex:1,
-        width:"100%",
-        margin:5,
-        flexDirection:'row',
-        backgroundColor:'#fff',
-        alignItens:'center',
-        justifyContent:'space-around',
+    container: {
+        flex: 1,
+        width: "100%",
+        margin: 5,
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        alignItens: 'center',
+        justifyContent: 'space-around',
     },
 
-titleAdd:{
-    color:"#fff",
-    fontSize:20,
-    fontWeight:"bold",
-    marginBottom:10,
-    alignContent:'center',
+    titleAdd: {
+        color: "#fff",
+        fontSize: 20,
+        fontWeight: "bold",
+        alignContent: 'center',
+        textTransform: "uppercase",
+    },
+    buttonProsseguir: {
+        alignItems: 'center',
+        backgroundColor: 'green',
+        borderRadius: 8,
+        padding: 15,
+        justifyContent: 'center',
+        width: "40%",
+        height: "10%",
+        top: "40%",
+    },
+
+    buttonAdd: {
+        alignItems: 'center',
+        backgroundColor: 'green',
+        borderRadius: 8,
+        padding: 15,
+        justifyContent: 'center',
+        width: "35%",
+        height: "10%",
+        top: "150%",
+    },
+
+    buttonRemove: {
+        alignItems: 'center',
+        backgroundColor: 'red',
+        padding: 15,
+        justifyContent: 'center',
+        width: "35%",
+        height: "10%",
+        top: "150%",
+        borderRadius: 8,
+    },
+
+    containerOperacao: {
+        width: "100%",
+        height: "100%",
+        flexDirection: 'column',
+        backgroundColor: 'w',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    botaoAtualizar: {
+        alignItems: 'center',
+        backgroundColor: 'green',
+        borderTopLeftRadius: '15%',
+        borderTopRightRadius: '15%',
+        justifyContent: 'center',
+        width: "100%",
+        height: 100,
+        position: 'absolute',
+        bottom: 0,
+    },
+
+    textInputQuantidade: {
+        alignItems: 'center',
+        backgroundColor: '#bebebe',
+        width: "80%",
+        height: 60,
+        padding: 15,
+        textAlign:"center",
+    },
+
+    textPrincipal: {
+        color: 'white',
+        textAlign: 'center',
+        fontWeight: "bold",
+        fontSize: 20,
+    },
+
+    containerTextPrincipal: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: "100%",
+        height: 150,
+        position: 'absolute',
+        top: 0,
+        backgroundColor: 'black',
+        fontSize: 20,
     
-
-},
-buttonProsseguir:{
-    alignItems:'center',
-     backgroundColor:'green',
-     borderRadius:8,
-     padding:15,
-     justifyContent:'center',
-     width:"40%",
-     height:"10%",
-         top:"40%",
- },
-
-
-
-buttonAdd:{
-   alignItems:'center',
-    backgroundColor:'green',
-    borderRadius:8,
-    padding:15,
-    justifyContent:'center',
-    width:"35%",
-    height:"10%",
-        top:"150%",
-},
-buttonRemove:{
-    alignItems:'center',
-    backgroundColor:'red',
-    borderRadius:8,
-    padding:15,
-    justifyContent:'center',
-    width:"35%",
-    height:"10%",
-    top:"150%",
-},
-
-
-containerOperacao:{
-    width:"120%",
-    height:"30%",
-    margin:5,
-    flexDirection:'column',
-    backgroundColor:'red',
-    justifyContent:'flex-start',
-    top:"3%",
-},
-
-botaoAtualizar:{
-    alignItems:'center',
-    backgroundColor:'green', 
-    borderRadius:5,
-    justifyContent:'center',
-    width:"80%",
-    height:"15%",
-    top:"200%",
-    alignContent:'flex-start',
-
-},
-
-textInputQuantidade:{
-    height:40,
-    alignItems:'center',
-    backgroundColor:'grey',
-    width:"80%",
-    height:"10%",
-    top:"120%",
-
-
-
-
-},
+    },
 
 
 });
 
-export{css};
+export { css };
--- views/Operacao.js
+++ views/Operacao.js
@@ -1,47 +1,59 @@
 //https://www.nuvemshop.com.br/ferramentas/gerador-qrcode GERADOR DE QRCODE
-import { Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { Text, View, TouchableOpacity, TextInput, KeyboardAvoidingView } from 'react-native';
 import { css } from '../assets/Css/Css';
 import React, { useState } from 'react';
+import { api } from '../services/api';
 
 export default function Operacao(props) {
     console.log(props);
     const [quantidade, setQuantidade] = useState('');
 
-//acao do botão:
-//const  reponse =   api.post("soma", {
- //id: props.route.params.idItem,
-  //  quantidade: quantidade,
-// });
+    //acao do botão:
+    //const  reponse =   api.post("soma", {
+    //id: props.route.params.idItem,
+    //  quantidade: quantidade,
+    // });
 
+    async function realizaOperacao() {
+        var operacao = props.route.params.Op; // variavel para direcionar a operacao correta na API soma ou subt
+        // Verifica a operação e realiza o cálculo correspondente
+        var metodo = operacao == "soma" ? "somar" : "subtrair"
 
-var operacao = props.route.params.Op; // variavel para direcionar a operacao correta na API soma ou subt
-// Verifica a operação e realiza o cálculo correspondente
-switch (operacao) {
-  case "soma":
-   
-  
-    break;
-  case "subt":
+        const reponse = await api.post(metodo, {
+            id: props.route.params.idItem,
+            quantidade: + quantidade,
+        });
 
-
-    break;
-
-}
+        if (reponse.data.success) {
+            // joga pra tela de sucesso
+           // props.navigation.navigate ('Sucesso')
+        }
+    }
     return (
-        <View style={css.containerOperacao}>
+        <KeyboardAvoidingView
+            enabled={Platform.OS === "ios"}
+            behavior="padding"
+            style={css.containerOperacao}
+        >
+
+
+            <View style={css.containerTextPrincipal} >
+                <Text style={css.textPrincipal}>STOCK MANAGER</Text>
+            </View>
             <TextInput style={css.textInputQuantidade}
                 value={quantidade}
-                keyboardType='numeric'
+                keyboardType="numeric"
+                returnKeyType='done'
                 onChangeText={(quantidade) => setQuantidade(quantidade)}
                 placeholder="Informe aqui a quantidade!"
+                placeholderTextColor={'black'}
             />
-            <Text>Teste: {props.route.params.Op} {props.route.params.idItem}
-                
-            </Text>
+      
 
-            <TouchableOpacity style={css.botaoAtualizar} > 
+            <TouchableOpacity style={css.botaoAtualizar} onPress={realizaOperacao} >
                 <Text style={css.titleAdd}>ATUALIZAR ESTOQUE</Text>
             </TouchableOpacity>
-        </View>
+
+        </KeyboardAvoidingView>
     );
 }
